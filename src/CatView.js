b31e0c61e545a9a8c348634ff19bc2b3d4c74ab3
jldaggs88 <daggs.jessa@gmail.com>
2021-03-03 10:43:06 -0600

    added date formatter

--- src/CatView.js
+++ src/CatView.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import DeleteModal from './DeleteModal'
 import EditModal from './EditModal';
+import { months } from './months';
 
 const CatView = ({ cat, deleteCat, editCat, setShowEdit, showEdit, showModal, setShowModal, allCats }) => {
-
   return (
     <div key={cat.id} className="cat-view">
       <div>
-        <div>{console.log('views', cat.viewsCount)}</div>
         <img src={cat.thumbnail} alt="" />
         <div>{cat.name}</div>
-        <div>{cat.birthDate}</div>
+        <div>{new Date(cat.birthDate).getDate() + " " + months[new Date(cat.birthDate).getMonth()] + " " + new Date(cat.birthDate).getFullYear()}</div>
         <div>{cat.ownerName}</div>
         <div>Number of views: {cat.viewsCount} times</div>
       </div>
       <div>
-        <button onClick={ () => setShowEdit(!showEdit)}>Edit</button>
-        <button onClick={ () => setShowModal(!showModal)}>
-          Delete
-        </button>
+        <div class="btn-toolbar" role="toolbar" aria-label="...">
+          <button type="button" class="btn btn-primary" onClick={ () => setShowEdit(!showEdit)}>
+            Edit
+          </button>
+        
+          <button type="button" class="btn btn-primary" onClick={ () => setShowModal(!showModal)}>
+            Delete
+          </button>
+        </div>
       </div>
       {showModal ? <DeleteModal showModal={showModal} setShowModal={setShowModal} deleteCat={deleteCat} cat={cat}/> : null}
       {showEdit ? <EditModal showEdit={showEdit} setShowEdit={setShowEdit} editCat={editCat} cat={cat} allCats={allCats} /> : null}
